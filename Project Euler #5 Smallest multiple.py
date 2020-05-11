c370e29db62d49fcb88aff394f2b616bad8e78ce
Omar Aljarrah <50204418+omar2682000@users.noreply.github.com>
2020-05-11 21:33:31 +0300

    Create Project Euler #5 Smallest multiple.py

---
+++ Project Euler #5 Smallest multiple.py
@@ -0,0 +1,19 @@
+from math import gcd 
+def lcm(a , b):return (a * b) // gcd(a , b);
+
+"""This Function called lcm returns the leatest common multiple of 2 numbers
+      << You can do it using recursion with the formula lcm(lcm(a , b) , c)
+            to avoid some kind of overflow , but it's okay as n's value 
+            can not exceed 40 >>
+"""
+
+for t in range(int(input())):
+    n = int(input());
+    for i in range(2 , n) : n = lcm(n , i);
+    print(n)
+    
+"""The kye behind this problem (which is a Number Theory problem by the way) is 
+        understanding both (lcm & gcd), nothig weird, just another simple boring
+        problem, but it's okay so far as we are still at first few problems, I 
+        think it's a natural thing so far.
+"""
