ebe69410da9c8f47def785571e44737bb8004a0e
Omar Aljarrah <50204418+omar2682000@users.noreply.github.com>
2020-05-11 05:07:41 +0300

    Update Project Euler #4 Largest palindrome product.py

--- Project Euler #4 Largest palindrome product.py
+++ Project Euler #4 Largest palindrome product.py
@@ -1,7 +1,10 @@
 T = int(input());
+
 def isPalindrome(string : str):
     if (string == string[::-1]):return True;
     else : return False;
+    """This Function checks if the string it is given is Palindrome or not"""
+    
 def generate():
     arr = []
     for i in range(100 , 1000):
@@ -10,10 +13,25 @@ def generate():
             if (isPalindrome(string2)):arr.append(i * j);
     arr.sort();
     return arr;
+    """This Function here uses a the first part of my Brute Force idea
+            to generate all possible solutions"""
 def find_answer(n , arr):
     for i in range(len(arr)):
         if arr[i] >= n : return i - 1;
+    """This function completes the other part of the idea to return an index for
+            the first palindromic number less than n"""
+    
 solutions = generate();
+
 for t in range(T):
     n = int(input());
     print(solutions[find_answer(n , solutions)])
+    
+"""
+Nothing weird about this problem, it migh look tricky at the first momment, 
+        but once you think about the limits you have, you will realize
+        that it's okay to go Brute Force (This solution won't pass all cases
+                                            if limits were sharper)
+        but anyway, that's a solved problem now
+        Also, a wise friend of mine said once : xD
+"""
